f6f4b5938afb9e5fc9faff8ee61cd79022082fe7
icoo08217 <icoo08216@gmail.com>
2022-08-08 13:13:28 +0900

    GetMapping , PostMapping , ResponseBody

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -12,6 +12,12 @@ public class MainController {
     // 아래 함수의 리턴값을 문자열화 해서 부라우저 응답의 바디에 담는다.
     public String index() {
         System.out.println("Hello"); // 콘솔에 출력
-        return "안녕하세요"; // 먼 미래에 브라우저에서 보여짐
+        return "안녕하세요~~~~~~~!!!!!!!!!!"; // 먼 미래에 브라우저에서 보여짐
+    }
+
+    @ResponseBody
+    @GetMapping("/sbb1")
+    public String showMake() {
+        return "<input type=\"text\">";
     }
 }
