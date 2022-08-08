ae97bd94c223e02f1830b51d081fa45368159947
icoo08217 <icoo08216@gmail.com>
2022-08-08 15:41:01 +0900

    cookie , session 개념 도입

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,8 +2,15 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.net.http.HttpRequest;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
@@ -32,7 +39,7 @@ public class MainController {
 
     @GetMapping("/minus")
     @ResponseBody
-    public String minus(@RequestParam int a , @RequestParam int b) {
+    public String minus(@RequestParam int a, @RequestParam int b) {
         return Integer.toString(a - b);
     }
 
@@ -40,5 +47,65 @@ public class MainController {
     @ResponseBody
     public String increase() {
         return Integer.toString(count++);
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public StringBuilder gugudan(@RequestParam int dan,
+                                 @RequestParam int limit) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= limit; i++) {
+            sb.append(dan).append(" * ").append(i).append(" = ").append(dan * i);
+            sb.append('\n');
+        }
+        return sb;
+    }
+
+//    @GetMapping("/gugudan")
+//    @ResponseBody
+//    public String showGugudan(Integer dan, Integer limit) {
+//        if (limit == null) {
+//            limit = 9;
+//        }
+//
+//        if (dan == null) {
+//            dan = 9;
+//        }
+//
+//        Integer finalDan = dan;
+//        return IntStream.rangeClosed(1, limit)
+//                .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
+//                .collect(Collectors.joining("<br>\n"));
+//    }
+
+    @GetMapping("/mbti/{name}")
+    @ResponseBody
+    public String mbti(@PathVariable String name) {
+        String rs = switch (name) {
+            case "홍길동" -> "INFP";
+            default -> "모름";
+        };
+
+        return rs;
+    }
+
+    @GetMapping("/saveSession/{name}/{value}")
+    @ResponseBody
+    public String saveSession(@PathVariable String name , @PathVariable String value, HttpServletRequest req){
+        HttpSession session = req.getSession();
+
+        // req => 쿠키 => JSESSIONID => 세션을 얻을 수 있다.
+
+        session.setAttribute(name, value);
+
+        return "세션변수 %s의 값이 %s(으)로 설정되었습니다.".formatted(name, value);
+    }
+
+    @GetMapping("/getSession/{name}")
+    @ResponseBody
+    public String getSession(@PathVariable String name , HttpSession session){
+        String value = (String) session.getAttribute(name);
+
+        return "세션변수 %s의 값은 %s입니다.".formatted(name, value);
     }
 }
