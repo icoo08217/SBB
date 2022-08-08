c18a8ddd33631599863f0bfeac26b4866c1fd922
icoo08217 <icoo08216@gmail.com>
2022-08-08 11:41:05 +0900

    @ResponseBody 사용

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,12 +2,16 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/sbb")
-    public void index() {
-        System.out.println("1234567");
+    @ResponseBody // 아래 함수의 리턴값을 그대로 브라우저에 표시함.
+    // 아래 함수의 리턴값을 문자열화 해서 부라우저 응답의 바디에 담는다.
+    public String index() {
+        System.out.println("Hello"); // 콘솔에 출력
+        return "안녕하세요"; // 먼 미래에 브라우저에서 보여짐
     }
 }
