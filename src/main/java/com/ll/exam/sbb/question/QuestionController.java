f597a1bc9b096c5304e1bf3dc46e9513325044b1
icoo08217 <icoo08216@gmail.com>
2022-08-16 11:27:24 +0900

    컨트롤러 서비스 도입

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,16 +1,22 @@
 package com.ll.exam.sbb.question;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+// 컨트롤러는 Repository가 있는지 몰라야한다.
+// 서비스는 웹브라우저라는 것이 이 세상에 존재하는지 몰라야 한다.
+// 세션은 어디서 다뤄야 할까 ? ==> 컨트롤러에서 다뤄야한다.
+// 리포지터리는 서비스의 존재를 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야한다.
+// SPRING DATA JPA는 MySQL을 몰라야한다.
+    // SPRING DATA JPA -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
 
     private final QuestionService questionService;
@@ -20,7 +26,7 @@ public class QuestionController {
     // 이 자리에 @ResponseBody가 없으면 resources/template/question_list.html 파일을 view 로 삼는다.
     public String list(Model model) {
 
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getList();
 
         // 미래에 실행될 question_list.html에서
         // "questionList"라는 이름으로 questionList
--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -1,13 +1,23 @@
 package com.ll.exam.sbb.question;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor // 생성자 주입
 public class QuestionService {
 
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
+
+//    필드 주입
+//    @Autowired
+//    private QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
 
     public Question findById(int id) {
         Question q1 = questionRepository.findById(2).get();
