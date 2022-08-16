1bd3fd42f21c8c26e9957078886317676549096f
icoo08217 <icoo08216@gmail.com>
2022-08-16 10:26:41 +0900

    타임리프 핫 리로드 설정

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,21 +1,31 @@
 package com.ll.exam.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
 
-    @Autowired
-    private QuestionService questionService;
+    private final QuestionService questionService;
+    private final QuestionRepository questionRepository;
 
     @RequestMapping("/question/list")
-    @ResponseBody
-    public String showList() {
-        questionService.findById(1);
+    // 이 자리에 @ResponseBody가 없으면 resources/template/question_list.html 파일을 view 로 삼는다.
+    public String list(Model model) {
 
-        return "HI";
+        List<Question> questionList = questionRepository.findAll();
+
+        // 미래에 실행될 question_list.html에서
+        // "questionList"라는 이름으로 questionList
+        model.addAttribute("questionList", questionList);
+
+        return "question_list";
     }
 }
